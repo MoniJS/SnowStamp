d8d4cb6ad04cb4da1c761020fcb35d6382ca762c
Suvajit <suvajit.c@outlook.com>
2019-05-26 21:10:11 +0530

    eslint 🙄

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ app.use(express.static(path.resolve(__dirname, "public")));
 const discordEpoch = 1420070400000;
 
 app.get("/time-stamp", (req, res) => {
-  const messageIdString = req.query.message;
-  const id = BigInt.asUintN(64, messageIdString);
-  const dateBits = Number(id >> 22n);
+    const messageIdString = req.query.message;
+    const id = BigInt.asUintN(64, messageIdString);
+    const dateBits = Number(id >> 22n);
 
-  const date = new Date(dateBits + discordEpoch);
-  const unix = (dateBits + discordEpoch);
-  const iso = new Date(unix).toISOString();
-  console.log(`Unix - ${unix}`); //unix = Unix date
-  console.log(`ISO - ${iso}`); //ISO 8601 iso = ISO 8601    
-  const time = moment.utc(date).format('MMMM Do YYYY, h:mm:ss a');
-  const timeFormated = `${time} UTC`;
-  res.send({timeFormated});
+    const date = new Date(dateBits + discordEpoch);
+    const unix = (dateBits + discordEpoch);
+    const iso = new Date(unix).toISOString();
+    console.log(`Unix - ${unix}`); //unix = Unix date
+    console.log(`ISO - ${iso}`); //ISO 8601 iso = ISO 8601    
+    const time = moment.utc(date).format('MMMM Do YYYY, h:mm:ss a');
+    const timeFormated = `${time} UTC`;
+    res.send({ timeFormated });
 });
 
 const port = process.env.PORT || 3000;
