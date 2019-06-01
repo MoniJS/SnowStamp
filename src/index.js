1a300501a6d8ef384fd87051a233401c8dfcbec7
Moni.S <40552237+MoniS10@users.noreply.github.com>
2019-05-31 20:59:13 -0400

    Added The logic for the time.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser");
-var moment = require('moment-timezone');
+let moment = require('moment-timezone');
 let path = require("path");
 let app = express();
 
@@ -17,11 +17,36 @@ app.get("/time-stamp", (req, res) => {
     const date = new Date(dateBits + discordEpoch);
     const unix = (dateBits + discordEpoch);
     const iso = new Date(unix).toISOString();
-    console.log(`Unix - ${unix}`); //unix = Unix date
-    console.log(`ISO - ${iso}`); //ISO 8601 iso = ISO 8601    
+    //console.log(`Unix - ${unix}`); unix = Unix date
+    //console.log(`ISO - ${iso}`); ISO 8601 iso = ISO 8601
     const time = moment.utc(date).format('MMMM Do YYYY, h:mm:ss a');
     const timeFormated = `${time} UTC`;
     res.send({ timeFormated });
+    res.send({ unix });
+});
+
+app.get("/unix-stamp", (req, res) => {
+    const messageIdString = req.query.message;
+    const id = BigInt.asUintN(64, messageIdString);
+    const dateBits = Number(id >> 22n);
+
+    const date = new Date(dateBits + discordEpoch);
+    const unix = (dateBits + discordEpoch);
+    const iso = new Date(unix).toISOString();
+    console.log(`Unix - ${unix}`); //unix = Unix date
+    res.send({ unix });
+});
+
+app.get("/iso-stamp", (req, res) => {
+    const messageIdString = req.query.message;
+    const id = BigInt.asUintN(64, messageIdString);
+    const dateBits = Number(id >> 22n);
+
+    const date = new Date(dateBits + discordEpoch);
+    const unix = (dateBits + discordEpoch);
+    const iso = new Date(unix).toISOString();
+    console.log(`ISO - ${iso}`); //ISO 8601 iso = ISO 8601
+    res.send({ iso });
 });
 
 const port = process.env.PORT || 3000;
